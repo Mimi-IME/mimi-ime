7df60b8f72f969627e75f0d18f835265933395ae
theeasternfurry <theeasternfurry@outlook.com>
2026-06-10 16:31:54 +0700

    fix: nix package wayland can not loaded

--- nix/default.nix
+++ nix/default.nix
@@ -9,17 +9,36 @@ rustPlatform.buildRustPackage {
   version = "1.0-unstable";
 
   src = ../.;
-  nativeBuildInputs = [ pkgs.pkg-config ];
+  nativeBuildInputs = [
+    pkgs.pkg-config
+    pkgs.autoPatchelfHook
+    pkgs.makeWrapper
+  ];
   buildInputs = [
     pkgs.wayland
     pkgs.libxkbcommon
+    pkgs.vulkan-loader
     pkgs.stdenv.cc.cc.lib
   ];
+
+  runtimeDependencies = [
+    pkgs.wayland
+    pkgs.libxkbcommon
+    pkgs.vulkan-loader
+  ];
+
   cargoLock = {
     lockFile = ../Cargo.lock;
   };
 
   postInstall = ''
+    wrapProgram $out/bin/mimi-ime \
+      --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [
+        pkgs.wayland
+        pkgs.libxkbcommon
+        pkgs.vulkan-loader
+      ]}
+
     # Install icon
     install -Dm644 images/mimi-ime-icon1.svg \
     $out/share/icons/hicolor/scalable/apps/mimi-ime.svg
