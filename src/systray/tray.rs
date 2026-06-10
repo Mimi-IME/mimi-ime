a11000ae6d7ca62c5d24ada0424977696e55cd6e
theeasternfurry <theeasternfurry@outlook.com>
2026-06-10 14:03:34 +0700

    feat: add settings gui

--- src/systray/tray.rs
+++ src/systray/tray.rs
@@ -1,14 +1,19 @@
-use crate::config::{APP_NAME, InputMode, settings::set_app_config};
+use crate::config::{
+    APP_NAME, InputMode,
+    settings::{ThemeMode, set_app_config},
+};
 use tokio::sync::mpsc::UnboundedSender;
 use tracing::info;
 
 pub enum TrayMessage {
     ModeChanged(InputMode),
+    OpenSettings,
 }
 
 #[derive(Debug)]
 pub struct MimiTray {
     pub current_mode: InputMode,
+    pub current_theme: ThemeMode,
     pub notifier: UnboundedSender<TrayMessage>,
 }
 
@@ -40,7 +45,7 @@ impl ksni::Tray for MimiTray {
                     let new_mode = modes[idx];
                     info!("Tray: mode changed to {:?}", new_mode);
                     this.current_mode = new_mode;
-                    set_app_config(new_mode);
+                    set_app_config(new_mode, this.current_theme);
                     this.notifier.send(TrayMessage::ModeChanged(new_mode)).ok();
                 }),
                 options: vec![
@@ -61,7 +66,18 @@ impl ksni::Tray for MimiTray {
             .into(),
             MenuItem::Separator,
             StandardItem {
-                label: "Quit".into(),
+                label: "Thiết lập".into(),
+                icon_name: "preferences-system".into(),
+                activate: Box::new(|this: &mut Self| {
+                    info!("Tray: settings requested");
+                    this.notifier.send(TrayMessage::OpenSettings).ok();
+                }),
+                ..Default::default()
+            }
+            .into(),
+            MenuItem::Separator,
+            StandardItem {
+                label: "Thoát".into(),
                 icon_name: "application-exit".into(),
                 activate: Box::new(|_| {
                     info!("Tray: quit requested");
