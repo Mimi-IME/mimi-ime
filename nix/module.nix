b8d4e439a3a42324e68eafd07d900dd0c3f0a00e
theeasternfurry <theeasternfurry@outlook.com>
2026-09-12 12:14:45 +0700

    feat: add desktop file for nix module

--- nix/module.nix
+++ nix/module.nix
@@ -4,6 +4,16 @@ defaultPackage:
 let
   cfg = config.programs."mimi-ime";
   isHM = options ? home.packages;
+
+  desktopItem = pkgs.makeDesktopItem {
+    name = "mimi-ime";
+    desktopName = "Mimi IME";
+    comment = "Vietnamese Input Method";
+    exec = "systemctl --user start mimi-ime";
+    icon = "mimi-ime";
+    terminal = false;
+    categories = [ "Utility" "InputMethod" ];
+  };
 in {
   options.programs."mimi-ime" = {
     enable = lib.mkEnableOption "Mimi IME";
@@ -15,35 +25,40 @@ in {
   };
 
   config = lib.mkIf cfg.enable (if isHM then {
-    home.packages = [ cfg.package ];
+    home.packages = [ cfg.package desktopItem ];
+
     systemd.user.services."mimi-ime" = {
       Unit = {
         Description = "Mimi IME";
         After = [ "graphical-session.target" ];
+        StartLimitBurst = 5;
+        StartLimitIntervalSec = 30;
       };
       Install.WantedBy = [ "graphical-session.target" ];
       Service = {
         ExecStart = "${cfg.package}/bin/mimi-ime";
         Restart = "on-failure";
         RestartSec = 1;
-        StartLimitBurst = 5;
-        StartLimitIntervalSec = 30;
       };
     };
   } else {
-    environment.systemPackages = [ cfg.package ];
+    environment.systemPackages = [ cfg.package desktopItem ];
+
     systemd.user.services."mimi-ime" = {
       description = "Mimi IME";
       wantedBy = [ "graphical-session.target" ];
       after = [ "graphical-session.target" ];
+      unitConfig = {
+        StartLimitBurst = 5;
+        StartLimitIntervalSec = 30;
+      };
       serviceConfig = {
         ExecStart = "${cfg.package}/bin/mimi-ime";
         Restart = "on-failure";
         RestartSec = 1;
-        StartLimitBurst = 5;
-        StartLimitIntervalSec = 30;
       };
     };
+
     warnings = [
       "programs.mimi-ime: add 'input-method { enable; }' to your compositor config to activate the IME."
     ];
